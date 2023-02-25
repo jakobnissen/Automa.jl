a747986b86d9276610f1b84e2e0385e5274277b8
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-02-25 15:43:15 +0100

    Remove Stream module

--- src/Automa.jl
+++ src/Automa.jl
@@ -1,6 +1,7 @@
 module Automa
 
 using Printf: @sprintf
+using TranscodingStreams: TranscodingStream, NoopStream
 import ScanByte: ScanByte, ByteSet
 
 # Encode a byte set into a sequence of non-empty ranges.
@@ -36,11 +37,10 @@ include("dot.jl")
 include("memory.jl")
 include("codegen.jl")
 include("tokenizer.jl")
-include("Stream.jl")
+include("stream.jl")
 
 const RE = Automa.RegExp
 using .RegExp: @re_str, opt, rep, rep1
-using .Stream: generate_reader, generate_io_validator
 
 # This list of exports lists the API
 export RE,
--- src/stream.jl
+++ src/stream.jl
@@ -1,15 +1,4 @@
 """
-Streaming Interface of Automa.jl.
-
-NOTE: This module is still experimental. The behavior may change without
-deprecations.
-"""
-module Stream
-
-import Automa
-import TranscodingStreams: TranscodingStream, NoopStream
-
-"""
     generate_reader(funcname::Symbol, machine::Automa.Machine; kwargs...)
 
 Generate a streaming reader function of the name `funcname` from `machine`.
@@ -176,5 +165,3 @@ function generate_io_validator(funcname::Symbol, machine::Automa.Machine, goto::
         $(funcname)(io::$(IO)) = $(funcname)($(NoopStream)(io))
     end 
 end
-
-end  # module
