08b29cfc64c6b66219fedc8b8b839fee63292fe0
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-03-01 15:04:57 +0100

    WIP: Tokenizer

--- src/tokenizer.jl
+++ src/tokenizer.jl
@@ -1,124 +1,121 @@
-# Tokenizer
-# =========
+# Currently, actions are added to final byte. This usually inhibits SIMD,
+# because the end position must be updated every byte.
+# It would be faster to add actions to :exit, but then the action will not actually
+# trigger when a regex is exited with an "invalid byte" - the beginning of a new regex.
+# I'm not quite sure how to handle this.
+"""
+    make_tokenizer(
+        funcname::Symbol, tokens::Vector{Symbol, RE};
+        goto=true, unambiguous=false
+    )
 
-struct Tokenizer
-    machine::Machine
-    actions_code::Vector{Tuple{Symbol,Expr}}
-end
+Create code which when evaluated, defines a function called `funcname`.
+This function will read a buffer (string, `Vector{UInt8}` etc) and return a vector
+of 3-tuples of integers:
+* The first is the 1-based starting index of the token in the buffer
+* The second is the length of the token in bytes
+* The third is the token kind: The index in the input list `tokens`.
+Any actions inside the input regexes will be ignored.
 
-function compile(tokens::AbstractVector{Pair{RegExp.RE,Expr}}; optimize::Bool=true)
-    start = NFANode()
-    final = NFANode()
-    actions = Dict{Symbol,Action}()
-    for i in 1:lastindex(tokens)
-        # HACK: place token exit actions after any other actions
-        action = Action(Symbol(:__token, i), 10000 - i)
-        actions[action.name] = action
-    end
-    actions_code = Tuple{Symbol,Expr}[]
-    for (i, (re, code)) in enumerate(tokens)
-        re′ = RegExp.shallow_desugar(re)
-        push!(get!(() -> Symbol[], RegExp.actions!(re′), :enter), :__token_start)
-        name = Symbol(:__token, i)
-        push!(get!(() -> Symbol[], RegExp.actions!(re′), :final), name)
-        nfa = re2nfa(re′, actions)
-        push!(start.edges, (Edge(eps), nfa.start))
-        push!(nfa.final.edges, (Edge(eps), final))
-        push!(actions_code, (name, code))
-    end
-    nfa = NFA(start, final)
-    dfa = nfa2dfa(remove_dead_nodes(nfa))
-    if optimize
-        dfa = remove_dead_nodes(reduce_nodes(dfa))
-    end
-    return Tokenizer(dfa2machine(dfa), actions_code)
-end
+The keyword `unambiguous` decides which of multiple matching tokens is emitted:
+If `false` (default), the longest token is emitted. If multiple tokens have the
+same length, the one with the highest index is returned.
+If `true`, `make_tokenizer` will error if any possible input text can be broken
+ambiguously down into tokens.
 
-function generate_init_code(tokenizer::Tokenizer)
-    # TODO: deprecate this?
-    return generate_init_code(CodeGenContext(), tokenizer)
-end
+If `goto` (default), use the faster, but more complex goto code generator.
 
-function generate_init_code(ctx::CodeGenContext, tokenizer::Tokenizer)
-    quote
-        $(ctx.vars.p)::Int = 1
-        $(ctx.vars.p_end)::Int = sizeof($(ctx.vars.data))
-        $(ctx.vars.is_eof)::Bool = true
-        $(ctx.vars.ts)::Int = 0
-        $(ctx.vars.te)::Int = 0
-        $(ctx.vars.cs)::Int = 1
-    end
-end
+# Example:
+```
+julia> make_tokenizer(:foo, [:a => re"ab+", :b => re"a"]) |> eval;
 
-function generate_exec_code(ctx::CodeGenContext, tokenizer::Tokenizer, actions=nothing)
-    if actions === nothing
-        actions = Dict{Symbol,Expr}()
-    elseif actions == :debug
-        actions = debug_actions(tokenizer.machine)
-    elseif isa(actions, AbstractDict{Symbol,Expr})
-        actions = copy(actions)
+julia> foo("abbbabaaababa")
+7-element Vector{Tuple{Int64, Int32, UInt32}}:
+ (1, 4, 0x00000001)
+ (5, 2, 0x00000001)
+ (7, 1, 0x00000002)
+ (8, 1, 0x00000002)
+ (9, 2, 0x00000001)
+ (11, 2, 0x00000001)
+ (13, 1, 0x00000002)
+```
+"""
+function make_tokenizer(
+    funcname::Symbol,
+    tokens::Vector{Pair{Symbol, RegExp.RE}};
+    goto::Bool=true,
+    unambiguous=false
+)
+    ctx = if goto
+        Automa.CodeGenContext(generator=:goto)
     else
-        throw(ArgumentError("invalid actions argument"))
+        Automa.DefaultCodeGenContext
     end
-    actions[:__token_start] = :($(ctx.vars.ts) = $(ctx.vars.p))
-    for (i, (name, _)) in enumerate(tokenizer.actions_code)
-        actions[name] = :(t = $(i); $(ctx.vars.te) = $(ctx.vars.p))
+    vars = ctx.vars
+    symbols = map(first, tokens)
+    if !allunique(symbols)
+        error("Names of tokens must be unique")
     end
-    return generate_table_code(ctx, tokenizer, actions)
-end
-
-function generate_table_code(ctx::CodeGenContext, tokenizer::Tokenizer, actions::AbstractDict{Symbol,Expr})
-    action_dispatch_code, set_act_code = generate_action_dispatch_code(ctx, tokenizer.machine, actions)
-    trans_table = generate_transition_table(tokenizer.machine)
-    getbyte_code = generate_getbyte_code(ctx)
-    cs_code = :($(ctx.vars.cs) = $(trans_table)[($(ctx.vars.cs) - 1) << 8 + $(ctx.vars.byte) + 1])
-    eof_action_code = generate_eof_action_code(ctx, tokenizer.machine, actions)
-    token_exit_code = generate_token_exit_code(tokenizer)
+    if :__enter_token ∈ symbols
+        error("Token symbol cannot be named `:__enter_token`")
+    end
+    tokens = map(tokens) do (symbol, regex)
+        regex = onfinal!(RegExp.strip_actions(regex), Symbol(:__token_, symbol))
+        symbol => onenter!(regex, :__enter_token)
+    end
+    # We intentionally set unambiguous=true. With the current construction of
+    # this tokenizer, this will cause the longest token to be matched, i.e. for
+    # the regex "ab" and "a", the text "ab" will emit only the "ab" regex.
+    # Requiring the user to pass in
+    machine = compile(RegExp.RE(:alt, map(last, tokens)); unambiguous=unambiguous)
+    actions = Dict{Symbol, Expr}(
+        :__enter_token => :(start = $(vars.p)),
+    )
+    for (i, symbol) in enumerate(symbols)
+        actions[Symbol(:__token_, symbol)] = quote
+            stop = $(vars.p)
+            token = $(UInt32(i))
+        end
+    end
     return quote
-        $(ctx.vars.mem) = $(SizedMemory)($(ctx.vars.data))
-        # Initialize token and token start to 0 - no token seen yet
-        t = 0
-        $(ctx.vars.ts) = 0
-        # In a loop: Get input byte, set action, update current state, execute action
-        while $(ctx.vars.p) ≤ p_end && $(ctx.vars.cs) > 0
-            $(getbyte_code)
-            $(set_act_code)
-            $(cs_code)
-            $(action_dispatch_code)
-            $(ctx.vars.p) += 1
-        end
-        if $(ctx.vars.is_eof) && $(ctx.vars.p) > $(ctx.vars.p_end)
-            # If EOF and in accept state, run EOF code and set current state to 0
-            # meaning accept state
-            if $(generate_final_state_mem_code(ctx, tokenizer.machine))
-                $(eof_action_code)
-                $(ctx.vars.cs) = 0
-            # Else, if we're not already in a failed state (cs < 0), then set cs to failed state
-            elseif $(ctx.vars.cs) > 0
-                $(ctx.vars.cs) = -$(ctx.vars.cs)
-            end
-        end
-        # If in a failed state, reset p to where it failed, since it was
-        # incremented immediately after the state transition
-        if $(ctx.vars.cs) < 0
-            $(ctx.vars.p) -= 1
-        end
-        if t > 0 && ($(ctx.vars.cs) ≤ 0 || $(ctx.vars.p) > p_end ≥ 0)
-            $(token_exit_code)
-            $(ctx.vars.p) = $(ctx.vars.te) + 1
-            if $(ctx.vars.cs) != 0
-                $(ctx.vars.cs) = 1
-            end
-        end
-    end
-end
+        function $(funcname)(data)
+            $(generate_init_code(ctx, machine))
+            tokens = Vector{Tuple{Int, Int32, UInt32}}(undef, 1024)
+            n_tokens = UInt(0)
+            stop = 0
+            while $(vars.cs) == 1
+                start = 0
+                token = UInt32(0)
+                $(generate_exec_code(ctx, machine, actions))
+                # If a full token has been observed, we accept error (either EOF),
+                # or a mismatched byte, and emit the token.
+                if !iszero(token)
+                    n_tokens += UInt(1)
+                    if n_tokens > length(tokens)
+                        resize!(tokens, n_tokens + UInt(1023))
+                    end
+                    @inbounds tokens[n_tokens] = (start, (stop-start+1)%Int32, token)
+                    $(vars.cs) = 1
 
-function generate_token_exit_code(tokenizer::Tokenizer)
-    i = 0
-    default = :()
-    return foldr(default, reverse(tokenizer.actions_code)) do name_code, els
-        _, code = name_code
-        i += 1
-        Expr(:if, :(t == $(i)), code, els)
+                    # Reset p, in case a token was matched far back, and we're
+                    # currently in the middle of another token which then turned
+                    # out not to match. I.e. re"a*b", re"a", match "aaa".
+                    # It reads past index 1 in case it matches the first regex, but
+                    # then it doesn't match so it need to reset after second regex
+                    $(vars.p) = stop + 1
+                # If we reach an error without having seen a token, there are two
+                # options: We just reached EOF without ever starting a token, indicating
+                # ordinary and expected EOF, or else we reached an invalid byte, or
+                # EOF in the middle of a token
+                else
+                    if $(vars.p) > $(vars.p_end) && iszero(start)
+                        break
+                    else
+                        $(generate_input_error_code(DefaultCodeGenContext, machine))
+                    end
+                end
+            end
+            return resize!(tokens, n_tokens)
+        end
     end
 end
